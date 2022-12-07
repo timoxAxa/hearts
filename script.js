f444a0154cde55fdf4b408d32d33575b08dd37ed
timoxAxa <119561170+timoxAxa@users.noreply.github.com>
2022-12-07 16:32:19 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+
+let strr = 'aaa@bbb@ccc'
+console.log(
+    strr
+);
+console.log(
+    strr.replace(/@/g, '!')
+);
+
+let str = 'HELLO WORLD'
+console.log(str);
+console.log(
+    str.toLowerCase()
+);
